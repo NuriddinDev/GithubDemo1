50e0b2bf906be4fda8bcf409938d2eefb461cfc3
Nuriddin <nuriddindev@gmail.com>
2021-07-17 22:39:23 +0500

    i did my task

--- GithubDemo1/SceneDelegate.swift
+++ GithubDemo1/SceneDelegate.swift
@@ -13,11 +13,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        callHomeController()
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
+    
+    func callHomeController() {
+            let vc:HomeViewController = HomeViewController(nibName: "HomeViewController", bundle: nil)
+            if self.window == nil {
+                self.window = UIWindow(frame: UIScreen.main.bounds)
+            }
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+        }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
